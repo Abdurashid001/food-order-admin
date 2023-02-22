082753d4a54de856b7b47147309bd51e579f0fed
Abdurashid001 <106978813+Abdurashid001@users.noreply.github.com>
2023-02-22 15:28:47 +0500

    Add files via upload

---
+++ food-order/admin/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Food order website-main page</title>
+    <link rel="stylesheet" href="/../css/admin.css">
+</head>
+<body>
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Order</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="main-content">
+        <div class="wrapper">Main content goes here</div>
+    </div>
+    <div class="footer">
+        <div class="wrapper">
+            <div class="text-center">Footer goes here</div>
+        </div>
+    </div>
+</body>
+</html>
